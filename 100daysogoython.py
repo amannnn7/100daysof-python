d75097600fe1ab823b11aad6703274b7382d76a6
Aman <141206845+amannnn7@users.noreply.github.com>
2025-04-04 14:22:05 +0530

    Add files via upload

---
+++ 100daysogoython.py
@@ -0,0 +1,41 @@
+#print("hello world")
+#this piece of code in programming is called stringswhich consist of character.
+
+#print("hello world\nhello world\nhello world")
+#print("hello + Aman")
+#this is called escape sequence in programming
+
+# print("hello"+ "aman")
+# # so now how we can add space in between
+# print("hello" + " " + "aman")
+
+# INPUT FUNCTION 
+# main function to take input from user
+#print("hello " + input("what is your name"))
+
+#write a program that print the number of character in a user name 
+#print(len(input("what is your name")))
+
+#VARIABLE IN PYTHON
+# name = input("what is your name")
+# print(name)  # prints the name of the user
+
+# name = input("what is your name")
+# length = len(name)
+# print(length)
+
+# WAP that switches the values stored in the variable a and b
+a = 5
+b = 10
+print("value of a before swapping: ", a)
+print("value of b before swapping: ", b)
+a = b
+b = a
+print("value of a after swapping: ", a)
+print("value of b after swapping: ", b)
+
+
+
+  
+
+
